ff801e646d5943463c36a4bf025e1e797b8bf0b4
Kevin Trogant <greenfoxlight.42@gmail.com>
2017-11-01 19:35:22 +0100

    Matlab 2 aufgabe 3

--- MatLab/2/wuerfelbecher.m
+++ MatLab/2/wuerfelbecher.m
@@ -1,3 +1,5 @@
-function [augensumme] = wuerfelbecher(wuerfel, wuerfe, max_augenziffer)
-
+function [augensumme] = wuerfelbecher(wuerfel, wuerfe, max_augenzahl)
+R = 1 + (max_augenzahl - 1) * rand(wuerfel, wuerfe);
+R = ceil(R);
+augensumme = sum(R, 1);
 end
