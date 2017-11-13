57bf95fbd681a51bcd0d48c2a159664e5e9c8354
Kevin Trogant <greenfoxlight.42@gmail.com>
2017-11-13 15:07:55 +0100

    Praktikum Blatt 2 Aufgabe 4

---
+++ MatLab/2/aufgabe4.m
@@ -0,0 +1,18 @@
+
+y1 = wuerfelbecher(5, 666, 7);
+y2 = wuerfelbecher(5, 66666, 7);
+y3 = wuerfelbecher(5, 6666666, 7);
+
+f = figure;
+p1 = subplot(1, 3, 1);
+histogram(y1);
+p2 = subplot(1, 3, 2);
+histogram(y2);
+p3 = subplot(1, 3, 3);
+histogram(y3);
+
+function [augensumme] = wuerfelbecher(wuerfel, wuerfe, max_augenzahl)
+R = 1 + (max_augenzahl - 1) * rand(wuerfel, wuerfe);
+R = ceil(R);
+augensumme = sum(R, 1);
+end
