70b6ba4caca21d62e924e8fec2d2814ed81b310e
Hoop77 <p.badenhoop@gmx.de>
2018-01-23 23:11:25 +0100

    37 done 38 done

--- MatLab/Scripts/Haar.m
+++ MatLab/Scripts/Haar.m
@@ -1,6 +1,6 @@
 function H = Haar(Rg)
     v1 = [1 1];
-    v2 = [1 - 1];
+    v2 = [1 -1];
     H = [v1; v2];
     i = 2;
     while i < Rg
@@ -9,8 +9,3 @@ function H = Haar(Rg)
     end;
     H = H / sqrt(i);
 return
-
-s = [1; 2; 3; 1; -1; -1; -1; 0]
-DHT = Haar(8)
-s1 = DHT * s
-s2 = transpose(DHT) * s
